ee4ef30a69e845dfd212ba431793a7e610b3597c
Timothy Jay <timothy.jay@dymaptic.com>
2021-11-26 17:39:31 -0800

    Based

--- ConsoleSandbox/Program.cs
+++ ConsoleSandbox/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using Jay.Text;
+
+Test.Thing("Id = {0}", 3);
+Test.Thing($"id = {3}");
+
+
+
+Console.WriteLine("Press Enter to close");
+Console.ReadLine();
+
+static class Test
+{
+    static Test()
+    {
+
+    }
+
+
+    public static void Thing(RawString format, params object?[] args)
+    {
+
+    }
+
+    // public static void Thing(string? format)
+    // {
+    //
+    // }
+    //
+    // public static void Thing(string? format, params object?[] args)
+    // {
+    //
+    // }
+
+    public static void Thing(FormattableString format)
+    {
+
+    }
+}
