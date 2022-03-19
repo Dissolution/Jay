a060183d09c1383fe5798fba73ec3b03660f119b
Timothy Jay <Timothy.Jay@dymaptic.com>
2022-03-19 16:08:41 -0700

    Resting

--- ConsoleSandbox/Scratch.cs
+++ ConsoleSandbox/Scratch.cs
@@ -4,29 +4,3 @@ using Jay.Conversion;
 
 namespace ConsoleSandbox;
 
-
-
-public interface IParser<TOut> : IParser
-{
-    Type IParser.OutputType => typeof(TOut);
-
-    Result IParser.TryParse(ReadOnlySpan<char> text, [NotNullWhen(true)] out object? obj, ParseOptions options)
-    {
-        Result result = TryParse(text, out TOut? value, options);
-        if (!result)
-        {
-            obj = null;
-            return result;
-        }
-        else
-        {
-            obj = value!;
-            return true;
-        }
-    }
-
-    Result TryParse(ReadOnlySpan<char> text, [NotNullWhen(true)] out TOut? value, ParseOptions options = default);
-
-    Result TryParse(string? text, [NotNullWhen(true)] out TOut? value, ParseOptions options = default)
-        => TryParse(text.AsSpan(), out value, options);
-}
