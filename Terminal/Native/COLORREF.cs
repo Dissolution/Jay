d9aed7148ecdeb54de9eaf67e99913ca713d9445
Timothy Jay <TimothyRJay@gmail.com>
2022-06-26 19:05:48 -0700

    Leftovers

---
+++ Terminal/Native/COLORREF.cs
@@ -0,0 +1,88 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+
+namespace Jay.Terminal.Native;
+
+/// <summary>
+/// Win32/GDI Color Structure
+/// </summary>
+/// <see cref="https://docs.microsoft.com/en-us/windows/win32/gdi/colorref"/>
+[StructLayout(LayoutKind.Explicit, Size = 4)]
+internal struct COLORREF 
+{
+    [FieldOffset(0)]
+    public byte R;
+    [FieldOffset(1)]
+    public byte G;
+    [FieldOffset(2)]
+    public byte B;
+
+    [FieldOffset(0)]
+    public uint Value;
+
+    public COLORREF(byte r, byte g, byte b) 
+    {
+        Value = 0;
+        R = r;
+        G = g;
+        B = b;
+    }
+
+    public COLORREF(uint value) 
+    {
+        R = 0;
+        G = 0;
+        B = 0;
+        Value = value & 0x00FFFFFF;
+    }
+
+    public COLORREF(Color color)
+    {
+        Value = (uint) color.ToArgb();
+        R = color.R;
+        G = color.G;
+        B = color.B;
+        // R = G = B = 0;
+        // Value = (uint)color.R + (((uint)color.G) << 8) + (((uint)color.B) << 16);
+    }
+
+    public Color ToColor()
+    {
+        return Color.FromArgb(R, G, B);
+    }
+
+    public bool Equals(COLORREF colorref) => this.Value == colorref.Value;
+
+    public bool Equals(Color color)
+    {
+        return this.R == color.R &&
+               this.G == color.G &&
+               this.B == color.B;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is COLORREF colorref) return Equals(colorref);
+        if (obj is Color color) return Equals(color);
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return (int)Value;
+    }
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        // 0x00BBGGRR
+        return $"0x{Value:X8}";
+    }
+
+    public static implicit operator Color(COLORREF colorref) => colorref.ToColor();
+    public static implicit operator COLORREF(Color color) => new COLORREF(color);
+    public static bool operator ==(COLORREF x, COLORREF y) => x.Equals(y);
+    public static bool operator !=(COLORREF x, COLORREF y) => !(x == y);
+}
---
+++ Terminal/Native/TerminalColor.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+
+namespace Jay.Terminal.Native;
+
+[Flags]
+public enum TerminalColor : byte
+{
+    Black =       0b00000000, //  0  Black
+    DarkBlue =    0b00000001, //  1  Blue
+    DarkGreen =   0b00000010, //  2  Green
+    DarkCyan =    0b00000011, //  3  Cyan = Green | Blue
+    DarkRed =     0b00000100, //  4  Red
+    DarkMagenta = 0b00000101, //  5  Magenta = Red | Blue
+    DarkYellow =  0b00000110, //  6  Yellow = Red | Green
+    Gray =        0b00000111, //  7  Gray = Red | Green | Blue
+    DarkGray =    0b00001000, //  8  Intense Black
+    Blue =        0b00001001, //  9  Intense Blue
+    Green =       0b00001010, // 10  Intense Green
+    Cyan =        0b00001011, // 11  Intense Cyan = Intense | Green | Blue
+    Red =         0b00001100, // 12  Intense Red
+    Magenta =     0b00001101, // 13  Intense Magenta = Intense | Red | Blue
+    Yellow =      0b00001110, // 14  Intense Yellow = Intense | Red | Green
+    White =       0b00001111, // 15  Intense Gray = Intense | Red | Green | Blue
+}
+
+public static class TerminalColors
+{
+    private static readonly Color[] _terminalColorToColorMap;
+    private const double _matchEpsilon = 0.001d;
+
+    static TerminalColors()
+    {
+        _terminalColorToColorMap = new Color[16]
+        {
+            /* Black       */ Color.FromArgb(0, 0, 0),
+            /* DarkBlue    */ Color.FromArgb(0, 0, 128),
+            /* DarkGreen   */ Color.FromArgb(0, 128, 0),
+            /* DarkCyan    */ Color.FromArgb(0, 128, 128),
+            /* DarkRed     */ Color.FromArgb(128, 0, 0),
+            /* DarkMagenta */ Color.FromArgb(128, 0, 128),
+            /* DarkYellow  */ Color.FromArgb(128, 128, 0),
+            /* Gray        */ Color.FromArgb(192, 192, 192),
+            /* DarkGray    */ Color.FromArgb(128, 128, 128),
+            /* Blue        */ Color.FromArgb(0, 0, 255),
+            /* Green       */ Color.FromArgb(0, 255, 0),
+            /* Cyan        */ Color.FromArgb(0, 255, 255),
+            /* Red         */ Color.FromArgb(255, 0, 0),
+            /* Magenta     */ Color.FromArgb(255, 0, 255),
+            /* Yellow      */ Color.FromArgb(255, 255, 0),
+            /* White       */ Color.FromArgb(255, 255, 255),
+        };
+    }
+
+    public static TerminalColor FindNearestTerminalColor(Color color)
+    {
+        // Simple dist check
+        TerminalColor closestColor = default;
+        double minDelta = double.MaxValue;
+            
+        for (int i = 0; i < _terminalColorToColorMap.Length; i++)
+        {
+            var checkColor = _terminalColorToColorMap[i];
+            var diff = Math.Pow(checkColor.R - color.R, 2d) +
+                       Math.Pow(checkColor.G - color.G, 2d) +
+                       Math.Pow(checkColor.B - color.B, 2d);
+            if (diff < minDelta)
+            {
+                closestColor = (TerminalColor) i;
+                minDelta = diff;
+            }
+        }
+        return closestColor;
+    }
+
+    public static Color ToColor(this TerminalColor terminalColor)
+    {
+        return _terminalColorToColorMap[(int) terminalColor];
+    }
+}
