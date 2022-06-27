0592a4e878fd1bb17b195cacb07b3644c29344ed
Timothy Jay <TimothyRJay@gmail.com>
2022-06-27 13:48:45 -0700

    Generic'd!

--- Geometry/Rectangle.cs
+++ Geometry/Rectangle.cs
@@ -1,64 +1,75 @@
-﻿using Jay.Hashing;
-using Jay.Reflection;
-using System;
-using System.Collections.Generic;
+﻿namespace Jay.Geometry;
 
-namespace Jay.Geometry;
-
-public readonly struct Rectangle : IEquatable<Rectangle>
+public readonly struct Rectangle<T> : IEquatable<Rectangle<T>>
+    where T : INumber<T>, IMinMaxValue<T>
 {
-    public static readonly Rectangle Empty = default;
+    public static bool operator ==(Rectangle<T> first, Rectangle<T> second) => first.Equals(second);
+    public static bool operator !=(Rectangle<T> first, Rectangle<T> second) => !first.Equals(second);
+
+    public static Rectangle<T> Empty { get; } = new Rectangle<T>();
         
-    public static Rectangle FromLTRB(int left, int top, int right, int bottom)
+    public static Rectangle<T> FromLTRB(T left, T top, T right, T bottom)
     {
-        return new Rectangle(left, top, Unmanaged.Add(left, right), Unmanaged.Add(top, bottom));
+        return new Rectangle<T>(left, top, left + right, top + bottom);
     }
 
-    public static Rectangle FromLTWH(int left, int top, int width, int height)
+    public static Rectangle<T> FromLTWH(T left, T top, T width, T height)
     {
-        return new Rectangle(left, top, width, height);
+        return new Rectangle<T>(left, top, width, height);
     }
 
-    public static Rectangle FromPointSize(Point location, Size size)
+    public static Rectangle<T> FromPointSize(Point<T> location, Size<T> size)
     {
-        return new Rectangle(location.X, location.Y, size.Width, size.Height);
+        return new Rectangle<T>(location.X, location.Y, size.Width, size.Height);
     }
 
-    public static Rectangle FromPoints(params Point[] points) => FromPoints((IEnumerable<Point>) points);
-    public static Rectangle FromPoints(IEnumerable<Point> points)
+    public static Rectangle<T> FromPoints(params Point<T>[] points) => FromPoints((IEnumerable<Point<T>>) points);
+    public static Rectangle<T> FromPoints(IEnumerable<Point<T>> points)
     {
-        int left = int.MaxValue;
-        int top = int.MaxValue;
-        int right = int.MinValue;
-        int bottom = int.MinValue;
-        foreach ((int x, int y) in points)
+        var left = T.MinValue;
+        var top = T.MaxValue;
+        var right = T.MinValue;
+        var bottom = T.MinValue;
+        foreach ((T x, T y) in points)
         {
-            if (x.CompareTo(left) < 0)
-                left = x;
-            if (x.CompareTo(right) > 0)
-                right = x;
-            if (y.CompareTo(top) < 0)
-                top = y;
-            if (y.CompareTo(bottom) > 0)
-                bottom = y;
+            if (x < left) left = x;
+            if (x > right) right = x;
+            if (y < top) top = y;
+            if (y > bottom) bottom = y;
+        }
+        return FromLTRB(left, top, right, bottom);
+    }
+    public static Rectangle<T> FromRectangles(params Rectangle<T>[] rectangles) => FromRectangles((IEnumerable<Rectangle<T>>) rectangles);
+    public static Rectangle<T> FromRectangles(IEnumerable<Rectangle<T>> rectangles)
+    {
+        var left = T.MinValue;
+        var top = T.MaxValue;
+        var right = T.MinValue;
+        var bottom = T.MinValue;
+        foreach (var rect in rectangles)
+        {
+            if (rect.Left < left) left = rect.Left;
+            if (rect.Top < top) top = rect.Top;
+            if (rect.Right > right) right = rect.Right;
+            if (rect.Bottom > bottom) bottom = rect.Bottom;
         }
         return FromLTRB(left, top, right, bottom);
     }
 
-    public readonly int X;
-    public readonly int Y;
-    public readonly int Width;
-    public readonly int Height;
+    public readonly T X;
+    public readonly T Y;
+    public readonly T Width;
+    public readonly T Height;
 
-    public int Left => X;
-    public int Top => Y;
-    public int Right => Left + Width;
-    public int Bottom => Top + Height;
+    public T Left => X;
+    public T Top => Y;
+    public T Right => Left + Width;
+    public T Bottom => Top + Height;
 
-    public Point Position => new Point(Left, Top);
-    public Size Size => new Size(Width, Height);
+    public Point<T> Position => new Point<T>(Left, Top);
+    public Size<T> Size => new Size<T>(Width, Height);
         
-    private Rectangle(int left, int top, int width, int height)
+    private Rectangle(T left, T top, T width, T height)
     {
         this.X = left;
         this.Y = top;
@@ -66,7 +77,7 @@ public readonly struct Rectangle : IEquatable<Rectangle>
         this.Height = height;
     }
 
-    public void Deconstruct(out int x, out int y, out int width, out int height)
+    public void Deconstruct(out T x, out T y, out T width, out T height)
     {
         x = X;
         y = Y;
@@ -74,16 +85,16 @@ public readonly struct Rectangle : IEquatable<Rectangle>
         height = Height;
     }
 
-    public bool Contains(Point point)
+    public bool Contains(Point<T> point)
     {
-        (int x, int y) = point;
+        (T x, T y) = point;
         return x.CompareTo(Left) >= 0 &&
                x.CompareTo(Right) <= 0 &&
                y.CompareTo(Top) >= 0 &&
                y.CompareTo(Bottom) <= 0;
     }
         
-    public bool Equals(Rectangle rectangle)
+    public bool Equals(Rectangle<T> rectangle)
     {
         return rectangle.Left == Left &&
                rectangle.Top == Top &&
@@ -93,12 +104,12 @@ public readonly struct Rectangle : IEquatable<Rectangle>
 
     public override bool Equals(object? obj)
     {
-        return obj is Rectangle rectangle && Equals(rectangle);
+        return obj is Rectangle<T> rectangle && Equals(rectangle);
     }
 
     public override int GetHashCode()
     {
-        return Hasher.Create(Left, Top, Right, Bottom);
+        return HashCode.Combine(X, Y, Width, Height);
     }
 
     public override string ToString()
--- Geometry/Size.cs
+++ Geometry/Size.cs
@@ -1,95 +1,64 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: System.Drawing.Size
-// Assembly: System.Drawing.Primitives, Version=5.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
-// MVID: D0804761-9704-49D2-9F69-9EE51CF26D7A
-// Assembly location: C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.4\System.Drawing.Primitives.dll
+﻿using System.Runtime.CompilerServices;
 
-using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-#nullable enable
 namespace Jay.Geometry;
 
-[TypeForwardedFrom("System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
-[TypeConverter("System.Drawing.SizeConverter, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
-[Serializable]
-public struct Size : IEquatable<Size>
+public readonly struct Size<T> : IEquatable<Size<T>>
+    where T : INumber<T>, IMinMaxValue<T>
 {
-  public static readonly Size Empty;
-  private int width;
-  private int height;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(Size<T> first, Size<T> second) => first.Width == second.Width && first.Height == second.Height;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(Size<T> first, Size<T> second) => first.Width != second.Width || first.Height != second.Height;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Size<T> operator +(Size<T> first, Size<T> second) => new Size<T>(first.Width + second.Width, first.Height + second.Height);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Size<T> operator -(Size<T> first, Size<T> second) => new Size<T>(first.Width - second.Width, first.Height - second.Height);
 
-  public Size(System.Drawing.Point pt)
-  {
-    this.width = pt.X;
-    this.height = pt.Y;
-  }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Size<T> operator -(Size<T> point) => new Size<T>(-point.Width, -point.Height);
 
-  public Size(int width, int height)
-  {
-    this.width = width;
-    this.height = height;
-  }
+    public static Size<T> Empty { get; } = new Size<T>();
+    
+    
+    public readonly T Width;
+    public readonly T Height;
 
-  public static implicit operator Jay.Geometry.SizeF(Size p) => new Jay.Geometry.SizeF((float) p.Width, (float) p.Height);
+    public bool HasArea => Width > T.Zero && Height > T.Zero;
+    
+    public Size(T width, T height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
 
-  public static Size operator +(Size sz1, Size sz2) => Size.Add(sz1, sz2);
+    public void Deconstruct(out T width, out T height)
+    {
+        width = this.Width;
+        height = this.Height;
+    }
 
-  public static Size operator -(Size sz1, Size sz2) => Size.Subtract(sz1, sz2);
+    /// <inheritdoc />
+    public bool Equals(Size<T> size)
+    {
+        return this.Width == size.Width && this.Height == size.Height;
+    }
 
-  public static Size operator *(int left, Size right) => Size.Multiply(right, left);
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        if (obj is Size<T> size) return Equals(size);
+        return false;
+    }
 
-  public static Size operator *(Size left, int right) => Size.Multiply(left, right);
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Width, Height);
+    }
 
-  public static Size operator /(Size left, int right) => new Size(left.width / right, left.height / right);
-
-  public static Jay.Geometry.SizeF operator *(float left, Size right) => Size.Multiply(right, left);
-
-  public static Jay.Geometry.SizeF operator *(Size left, float right) => Size.Multiply(left, right);
-
-  public static Jay.Geometry.SizeF operator /(Size left, float right) => new Jay.Geometry.SizeF((float) left.width / right, (float) left.height / right);
-
-  public static bool operator ==(Size sz1, Size sz2) => sz1.Width == sz2.Width && sz1.Height == sz2.Height;
-
-  public static bool operator !=(Size sz1, Size sz2) => !(sz1 == sz2);
-
-  public static explicit operator System.Drawing.Point(Size size) => new System.Drawing.Point(size.Width, size.Height);
-
-  [Browsable(false)]
-  public readonly bool IsEmpty => this.width == 0 && this.height == 0;
-
-  public int Width
-  {
-    readonly get => this.width;
-    set => this.width = value;
-  }
-
-  public int Height
-  {
-    readonly get => this.height;
-    set => this.height = value;
-  }
-
-  public static Size Add(Size sz1, Size sz2) => new Size(sz1.Width + sz2.Width, sz1.Height + sz2.Height);
-
-  public static Size Ceiling(Jay.Geometry.SizeF value) => new Size((int) System.Math.Ceiling((double) value.Width), (int) System.Math.Ceiling((double) value.Height));
-
-  public static Size Subtract(Size sz1, Size sz2) => new Size(sz1.Width - sz2.Width, sz1.Height - sz2.Height);
-
-  public static Size Truncate(Jay.Geometry.SizeF value) => new Size((int) value.Width, (int) value.Height);
-
-  public static Size Round(Jay.Geometry.SizeF value) => new Size((int) System.Math.Round((double) value.Width), (int) System.Math.Round((double) value.Height));
-
-  public override readonly bool Equals(object? obj) => obj is Size other && this.Equals(other);
-
-  public readonly bool Equals(Size other) => this == other;
-
-  public override readonly int GetHashCode() => HashCode.Combine<int, int>(this.Width, this.Height);
-
-  public override readonly string ToString() => "{Width=" + this.width.ToString() + ", Height=" + this.height.ToString() + "}";
-
-  private static Size Multiply(Size size, int multiplier) => new Size(size.width * multiplier, size.height * multiplier);
-
-  private static Jay.Geometry.SizeF Multiply(Size size, float multiplier) => new Jay.Geometry.SizeF((float) size.width * multiplier, (float) size.height * multiplier);
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return $"[{Width}x{Height}]";
+    }
 }
