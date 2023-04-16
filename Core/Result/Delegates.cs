57d9c4dc8d074b591d944cb11b38f2c16aadb38d
Timothy Jay <TimothyRJay@gmail.com>
2023-04-16 13:25:42 -0700

    Initial reset

--- Core/Result/Delegates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Jay.Result;
-
-/// <summary>
-/// A function that returns a <see cref="Result"/> and an <c>out</c> <paramref name="value"/>, similar to a <c>TryParse</c> operation
-/// </summary>
-/// <typeparam name="T">The <see cref="Type"/> of <paramref name="value"/> output.</typeparam>
-/// <returns>A <see cref="Result"/> for the operation</returns>
-public delegate Result ResultOutFunc<T>(out T value);
--- Dumping/IDumperT.cs
+++
@@ -1,8 +0,0 @@
-﻿using Jay.Dumping.Interpolated;
-
-namespace Jay.Dumping;
-
-public interface IDumper<in T> : IDumper
-{
-    void DumpTo(ref DumpStringHandler dumpHandler, T? value, DumpFormat format = default);
-}
